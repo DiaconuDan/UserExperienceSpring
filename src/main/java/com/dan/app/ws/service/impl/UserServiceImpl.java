d06e1839e557ebfaf18492119e83862538dd4f89
Diaconu Dan <diaconudann@gmail.com>
2018-09-03 22:33:19 +0300

    Started Sign-In implementation with Sprig Security Authentication Filter

--- src/main/java/com/dan/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/dan/app/ws/service/impl/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.dan.app.ws.service.impl;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -51,9 +54,12 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity userEntity =  userRepository.findUserByEmail(email) ;
+		
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>()) ;
 	}
 
 }
